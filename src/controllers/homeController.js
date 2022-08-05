d4df97723805b193cbc169144956920aece42ad4
danail2003 <ddobrev20@outlook.com>
2022-08-05 17:37:01 +0300

    Dynamic generating cubes

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,7 @@
+const cubes = require('../config/database.json');
+
 exports.index = (req, res) => {
-    res.render('index');
+    res.render('index', { cubes });
 };
 
 exports.about = (req, res) => {
