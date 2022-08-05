6aa1c40c400ec8ebecbbe0271a4887f567de3240
danail2003 <ddobrev20@outlook.com>
2022-08-05 16:43:42 +0300

    added views and controllers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const config = require('../config/config');
-
+const handlebars = require('express-handlebars');
+const config = require('./config/config');
+const routes = require('./config/routes');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Welcome world");
-});
+app.use('/static', express.static('static'));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.use(routes);
 
 app.listen(config.development, () => console.log(`App is listening on port ${config.development.port}`));
