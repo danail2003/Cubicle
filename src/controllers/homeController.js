8b103635c2ffe8993613755db3a78b857b5c87eb
danail2003 <ddobrev20@outlook.com>
2022-08-06 17:52:12 +0300

    Search functionality works

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const cubes = require('../config/database.json');
+const cubeService = require('../services/cubeService');
 
 router.get('/', (req, res) => {
+    const { search, to, from } = req.query;
+
+    const cubes = cubeService.getAll(search, from, to);
     res.render('index', { cubes });
 });
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,8 +3,26 @@ const fs = require('fs/promises');
 const path = require('path');
 const uuid = require('uuid');
 
+exports.getAll = (search = '', from = 0, to = 6) => {
+    if (from === '' || to === '') {
+        if (from === '') {
+            from = 0;
+        }
+
+        if (to === '') {
+            to = 6;
+        }
+    }
+
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+        .filter(x => x.difficultyLevel >= Number(from) && x.difficultyLevel <= Number(to));
+
+    return result;
+};
+
 exports.save = (cube) => {
-    cubes.push({id: uuid.v1(), ...cube});
+    cubes.push({ id: uuid.v1(), ...cube });
     return fs.writeFile(path.resolve('src/config', 'database.json'), JSON.stringify(cubes, '', 4), { encoding: 'utf-8' })
 }
 
